29664d76b89ce70a83ae6c0944b699abf409c299
Leandro Miranda Fahur Machado <leandrofahur@gmail.com>
2025-07-09 16:29:24 -0700

    removed the docs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+"use client";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">        
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -11,7 +12,8 @@ export default function Home() {
           width={180}
           height={38}
           priority
-        />
+        />        
+        
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
